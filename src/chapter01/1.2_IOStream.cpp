13ef6b27ca37b02184ed7b928874d2c2cf2aceaf
Bob <0haizhu0@gmail.com>
2017-06-24 18:28:14 +0800

    注释：标准库中的四个IO对象

--- src/chapter01/1.2_IOStream.cpp
+++ src/chapter01/1.2_IOStream.cpp
@@ -1,8 +1,17 @@
-//
-// Created by Bob on 2017/6/24.
-//
 #include <iostream>
 
+/**
+ * 输入和输出范例
+ * 不同于C程序的printf直接打印
+ *
+ * - 四个IO对象：
+ * -- cin 读作 see-in
+ * -- cout 读作 see-out
+ * -- ceeror 读作 see-error，用来输出警告和错误信息
+ * -- clog 读作 see-log，用来输出程序运行时的一般性信息
+ * @return
+ * Created by Bob on 2017/6/24.
+ */
 int main() {
   std::cout << "Please enter to numbers:" << std::endl;
   int v1 = 0, v2 = 0;

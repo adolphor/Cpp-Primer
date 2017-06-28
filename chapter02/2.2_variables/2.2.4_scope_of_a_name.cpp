7dfd88f193b19b8a43c33ed3139f6110a58f3dd4
Bob <0haizhu0@gmail.com>
2017-06-29 00:35:31 +0800

    名字的作用域

---
+++ chapter02/2.2_variables/2.2.4_scope_of_a_name.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/**
+ * 2.2.4 名字的作用域
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ */
+int reused = 42; // resued拥有全局作用域
+int main() {
+
+  // 作用域：和Java规则一样的啦~
+  int sum = 0; // 拥有块作用域
+  for (int val = 0; val <= 10; ++val) {
+    sum += val;
+  }
+
+  // 嵌套的作用域
+  std::cout << reused << " " << sum << std::endl;
+  int reused = 0; // 新建局部变量reused，覆盖了全局变量 reused
+  std::cout << reused << " " << sum << std::endl;  // 生效的是局部变量
+  std::cout << ::reused << " " << sum << std::endl; // 显示访问全局变量
+
+  return 0;
+
+}

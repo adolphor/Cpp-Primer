94566a36ee14348ccd5583bba527eec0bcf4e778
Bob <0haizhu0@gmail.com>
2017-06-30 22:18:49 +0800

    2.3.1 练习题

---
+++ chapter02/2.3_compound_types/2.3.1_P_217.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/**
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/6/30
+ */
+int main() {
+  int i, &ri = i; // i默认初始值为0？然后ri也为0？
+  std::cout << i << " " << ri << std::endl;
+  i = 5; // 都为5
+  std::cout << i << " " << ri << std::endl;
+  ri = 10; // 都为10
+  std::cout << i << " " << ri << std::endl;
+  return 0;
+}
+

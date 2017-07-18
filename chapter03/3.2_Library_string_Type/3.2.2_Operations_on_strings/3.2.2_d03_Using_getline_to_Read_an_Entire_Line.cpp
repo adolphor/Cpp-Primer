0bee381837f57918d4e6f1804d0e80e25c0fc9fd
Bob <0haizhu0@gmail.com>
2017-07-18 22:27:57 +0800

    3.2.2 string字符串上的操作：使用getline读取一整行

--- chapter03/3.2_Library_string_Type/3.2.2_Operations_on_strings/3.2.2_d03_Using_getline_to_Read_an_Entire_Line.cpp
+++ chapter03/3.2_Library_string_Type/3.2.2_Operations_on_strings/3.2.2_d03_Using_getline_to_Read_an_Entire_Line.cpp
@@ -14,8 +14,9 @@ using std::cout; using std::endl;
 
 int main() {
   string line;
+  // 触发getline函数返回的那个换行符实际上被丢弃掉了，得到的line对象中并不包含换行符
   while (getline(cin, line)) { // 注意cin参数的传递
-    cout << line << endl;
+    cout << line << endl; // 同以前一样，使用endl结束当前行并刷新显示缓冲区
   }
   return 0;
 }

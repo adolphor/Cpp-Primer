570808abf0f83a06ef42470070a191db8ac241ac
Bob <0haizhu0@gmail.com>
2017-07-24 20:51:00 +0800

    3.4.2 迭代器的运算

---
+++ chapter03/3.4_Introducing_Iterators/3.4.2_Iterator_Arithmetic.cpp
@@ -0,0 +1,17 @@
+/**
+ * 3.4.2 迭代器的运算
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/7/24
+ */
+int main(){
+  /**
+   * iter + n 迭代器向前移动n个元素，如果越界，指向容器尾元素的下一位置
+   * iter - n 得带起向后移动n个元素，如果越界，指向容器尾元素的下一位置
+   * iter += n 迭代器加法的复合赋值语句，将iter+n的结果赋值给iter
+   * iter -+ n 迭代器剪发的复合赋值语句，将iter-n的结果赋值给iter
+   * iter1 - iter2 两个迭代器之间间隔的位置，也就是iter2向前移动多少位置能够得到iter1
+   * >，>=，<，<=
+   */
+  return 0;
+}

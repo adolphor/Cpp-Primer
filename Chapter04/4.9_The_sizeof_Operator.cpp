8271d97fba0e21bebbb881c32e54d0d3dccdcca4
Bob <0haizhu0@gmail.com>
2017-07-29 17:50:25 +0800

    4.9 sizeof运算符

--- Chapter04/4.9_The_sizeof_Operator.cpp
+++ Chapter04/4.9_The_sizeof_Operator.cpp
@@ -21,5 +21,16 @@ int main(){
   sizeof *p; // p 所指类型的空间大小, 即 sizeof(Sales_data)
   sizeof data.revenue; // Sales_data 的 revenue 成员对应类型的大小
   sizeof Sales_data::revenue; // 另一种获取 revenue 大小的方式
+
+  /**
+   * sizeof运算符的结果部分依赖于其作用的类型：
+   * 1. char 结果 为1
+   * 2. 引用类型 结果是被引用对象所占空间大小
+   * 3. 指针 结果是指针本身所占空间大小
+   * 4. 解引用指针 结果是指针指向的对象所占空间大小，指针不需要有效
+   * 5. 数组 结果是数组中各元素所占空间大小之和
+   * 6. string 或 vector 只返回该类型固定部分的大小，不计算对象中的元素占用了多少空间
+   */
+
   return 0;
 }

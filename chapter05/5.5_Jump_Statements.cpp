b075a662298ae97f0fd1572967e2f2b7cfa4926a
Bob <0haizhu0@gmail.com>
2017-08-01 23:55:08 +0800

    5.5 跳转语句

---
+++ chapter05/5.5_Jump_Statements.cpp
@@ -0,0 +1,22 @@
+/**
+ * 5.5 跳转语句
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/1
+ */
+int main(){
+  /**
+   * 5.5.1 break语句
+   */
+
+  /**
+   * 5.5.2 continue语句
+   */
+
+  /**
+   * 5.5.3 goto语句
+   * 这个倒是Java中没有的关键字
+   */
+
+  return 0;
+}

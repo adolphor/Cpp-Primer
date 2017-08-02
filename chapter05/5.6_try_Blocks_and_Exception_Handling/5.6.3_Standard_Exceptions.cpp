872504585fdc929b131ab204c3f352a5808f3c12
Bob <0haizhu0@gmail.com>
2017-08-02 22:12:32 +0800

    5.6.3 标准异常

---
+++ chapter05/5.6_try_Blocks_and_Exception_Handling/5.6.3_Standard_Exceptions.cpp
@@ -0,0 +1,15 @@
+/**
+ * 5.6.3 标准异常
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/2
+ */
+int main(){
+  /**
+   * exception：定义了最通用的异常类，只报告异常的发生，不提供任何额外信息
+   * stdexcept：定义了几种常用的异常类。
+   * new => bad_alloc：后面12.1.2章节学习
+   * type_info => bad_cast：后面19.2章节学习
+   */
+  return 0;
+}

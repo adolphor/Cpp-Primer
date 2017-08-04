fa10411239d0e238d8b4e96916dbd17660d057f5
Bob <0haizhu0@gmail.com>
2017-08-04 19:56:57 +0800

    6.2 参数传递

---
+++ chapter06/6.2_Argument_Passing/6.2.0_introduce.cpp
@@ -0,0 +1,14 @@
+/**
+ * 6.2 参数传递
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/4
+ */
+int main(){
+  /**
+   * 参数传递跟Java一样，分为如下两种：
+   * 1. 值传递
+   * 2. 引用传递
+   */
+  return 0;
+}

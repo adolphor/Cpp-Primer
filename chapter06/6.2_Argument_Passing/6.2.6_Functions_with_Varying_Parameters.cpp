d0666e893d4668fee81b581e871fea4930062013
Bob <0haizhu0@gmail.com>
2017-08-10 22:26:45 +0800

    6.2.6 含有可变形参的函数

---
+++ chapter06/6.2_Argument_Passing/6.2.6_Functions_with_Varying_Parameters.cpp
@@ -0,0 +1,14 @@
+/**
+ * 6.2.6 含有可变形参的函数
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/10
+ */
+int main(){
+  /**
+   * 可变形参有两种方式：TODO
+   * 1. 使用名为 initializer_list 的标准库类型：实参数量未知，但类型相同时使用
+   * 2. 可变参数模板（16.4讲解）
+   */
+  return 0;
+}

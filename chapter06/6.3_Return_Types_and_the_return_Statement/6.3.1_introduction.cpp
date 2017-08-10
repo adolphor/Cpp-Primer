f6654a7c7b48351ab361696339eb4d1a3940e50d
Bob <0haizhu0@gmail.com>
2017-08-10 22:33:13 +0800

    6.3 返回类型和return语句

---
+++ chapter06/6.3_Return_Types_and_the_return_Statement/6.3.1_introduction.cpp
@@ -0,0 +1,12 @@
+/**
+ * 6.3 返回类型和return语句
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/10
+ */
+int main(){
+  /**
+   * return语句终止当前正在执行的函数，并将控制权返回到调用该函数的地方。
+   */
+  return 0;
+}

00f47fc57ae9680cabbdfc896b2fe03071354171
Bob <0haizhu0@gmail.com>
2017-08-10 22:37:33 +0800

    6.3.1 无返回值函数

---
+++ chapter06/6.3_Return_Types_and_the_return_Statement/6.3.1_Functions_with_No_Return_Value.cpp
@@ -0,0 +1,14 @@
+/**
+ * 6.3.1 无返回值函数
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/10
+ */
+int main(){
+  /**
+   * 1. 没有返回值的return用在void的函数中
+   * 2. 但void函数中return不是必须的，因为在函数末尾会隐式执行return
+   * 3. 在void函数中提前退出的时候可以使用return
+   */
+  return 0;
+}

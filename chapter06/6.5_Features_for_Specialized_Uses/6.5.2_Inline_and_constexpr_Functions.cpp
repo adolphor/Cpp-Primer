fc14673bf0e07e6ddbab9889b9291a94d40b240b
Bob <0haizhu0@gmail.com>
2017-08-14 23:05:34 +0800

    6.5.2 内联函数和constexpr函数

---
+++ chapter06/6.5_Features_for_Specialized_Uses/6.5.2_Inline_and_constexpr_Functions.cpp
@@ -0,0 +1,19 @@
+/**
+ * 6.5.2 内联函数和constexpr函数
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/14
+ */
+int main(){
+  /**
+   * 内联函数：添加inline关键字。
+   * 函数相比于表达式更容易阅读、复用，但比表达式开销大。
+   * 内联函数可避免函数调用的开销。
+   */
+
+  /**
+   * constexpr是指能用于常量表达式的函数。
+   */
+
+  return 0;
+}

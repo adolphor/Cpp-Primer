ca23c09e17193997b7b35c57f127db36f60f4e74
Bob <0haizhu0@gmail.com>
2017-08-14 22:51:05 +0800

    6.5 特殊用户语言特性

---
+++ chapter06/6.5_Features_for_Specialized_Uses/6.5.0_introduction.cpp
@@ -0,0 +1,15 @@
+/**
+ * 6.5 特殊用户语言特性
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/14
+ */
+int main(){
+  /**
+   * 介绍三种函数相关的特性：
+   * 1. 默认实参
+   * 2. 内联函数
+   * 3. constexpr函数
+   */
+  return 0;
+}

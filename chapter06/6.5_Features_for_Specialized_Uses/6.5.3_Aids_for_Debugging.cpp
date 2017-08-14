e1cbbdd3c4da5a0baab62406447ddde90bf727a7
Bob <0haizhu0@gmail.com>
2017-08-14 23:28:05 +0800

    6.5.3 调试帮助

---
+++ chapter06/6.5_Features_for_Specialized_Uses/6.5.3_Aids_for_Debugging.cpp
@@ -0,0 +1,16 @@
+/**
+ * 6.5.3 调试帮助
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/14
+ */
+int main(){
+  /**
+   * assert预处理宏
+   */
+
+  /**
+   * DEBUG预处理变量
+   */
+  return 0;
+}

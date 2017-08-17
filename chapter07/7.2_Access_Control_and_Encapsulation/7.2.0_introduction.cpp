e9e4c1be7c6ce1370c62a08a7f8e374e84cb18b4
Bob <0haizhu0@gmail.com>
2017-08-17 23:00:21 +0800

    7.2 访问控制与封装

---
+++ chapter07/7.2_Access_Control_and_Encapsulation/7.2.0_introduction.cpp
@@ -0,0 +1,14 @@
+/**
+ * 7.2 访问控制与封装
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/17
+ */
+int main(){
+  /**
+   * 使用class或struct关键字：
+   * class：默认成员权限是private
+   * struct：默认成员权限是public
+   */
+  return 0;
+}

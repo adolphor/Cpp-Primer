2eefe8a1df0f179ba18060be231b40cb6e5661c3
Bob <0haizhu0@gmail.com>
2017-08-25 19:41:37 +0800

    练习 1.1

---
+++ chapter01/1.1_Writing_a_Simple_Cpp_Program/1.1.1_P1.1_Compiling_and_Executing.cpp
@@ -0,0 +1,19 @@
+/**
+ * 练习 1.1
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/8/25
+ */
+
+#include <iostream>
+
+int main(){
+  /**
+   * 编译：g++ 1.1.1_P1.1_Compiling_and_Executing.cpp
+   * 指定编译器支持版本：g++ 1.1.1_P1.1_Compiling_and_Executing.cpp -std=c++11
+   * 指定编译后的文件名：g++ -o hello 1.1.1_P1.1_Compiling_and_Executing.cpp -std=c++11
+   * 运行文件：./a.out 或者 ./hello
+   */
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}

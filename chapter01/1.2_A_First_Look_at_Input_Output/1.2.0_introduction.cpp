eb1726f575a8b2b78d4e910b16ead100c6d17fcb
Bob <0haizhu0@gmail.com>
2017-08-26 20:28:58 +0800

    1.2 初始输入输出

--- chapter01/1.2_A_First_Look_at_Input_Output/1.2.0_introduction.cpp
+++ chapter01/1.2_A_First_Look_at_Input_Output/1.2.0_introduction.cpp
@@ -19,10 +19,19 @@ int main() {
   /**
    * 一个使用IO库的程序
    */
-  std::cout << "Please enter to numbers:" << std::endl;
+  std::cout << "Please enter to numbers:"  // 命名空间 std，作用域运算符 ::
+            << std::endl; // << 输出运算符
+  // 等价于下面两句：
+  // std::cout << "Please enter to numbers:";
+  // std::cout << std::endl;
+
   int v1 = 0, v2 = 0;
   // 顺序将输入值赋值到v1，v2，而且输入两个参数之后回车就自动往下运行（或者每输入一个数字回车一次）
-  std::cin >> v1 >> v2; // 不需要 std::endl
+  std::cin >> v1 >> v2; // >> 输入运算符
+  // 等价于下面两句
+  // std::cin >> v1;
+  // std::cin >> v2;
+
   std::cout << "The sum of " << v1 << " and " << v2
             << " is " << v1 + v2 << std::endl;
   return 0;

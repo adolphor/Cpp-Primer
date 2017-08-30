7dcbbcc32eda5286cc697b8e422ba5861fb4ea59
Bob <0haizhu0@gmail.com>
2017-08-30 22:35:24 +0800

    2.3.1 引用

--- chapter02/2.3_Compound_Types/2.3.1_References.cpp
+++ chapter02/2.3_Compound_Types/2.3.1_References.cpp
@@ -16,19 +16,22 @@ int main() {
    * 因此，引用个必须初始化。
    */
   int ival = 1024;
-  int &refVal = ival; // refVal指向ival，（是ival的另外一个名字）
+  int &refVal = ival; // refVal指向ival，（是ival的另外一个名字）；此处 & 是声明符的一部分，称为 类型修饰符
   // int &refVal2;   // 报错，引用必须初始化
-  std::cout << "ival=" << ival << ", refVal=" << refVal << std::endl;
+  std::cout << "ival = " << ival << std::endl; // 变量ival及其值
+  std::cout << "refVal = " << refVal << std::endl; // 变量refVal及其值
+  std::cout << "&refVal = " << &refVal << std::endl; // 此处 & 是解引用符，表示获取此变量的内存地址
 
   refVal = 2048; // 更改引用refVal的值，也就是更改ival的值，因为他俩绑定的是同一个对象
-  std::cout << "ival=" << ival << ", refVal=" << refVal << std::endl;
+  std::cout << "ival = " << ival << std::endl; // 值被更改
+  std::cout << "refVal = " << refVal << std::endl; // 值被更改
+  std::cout << "&refVal = " << &refVal << std::endl; // 但内存地址不变
 
   // 因为引用本身不是对象，所以不能定义引用的引用
   /**
    * 所有引用的类型都要和与之绑定的对象严格匹配，引用只能绑定在对象上，
    * 不能与字面值或某个表达式的计算结果绑定在一起，原因在2.4.1讲述。
    */
-
   return 0;
 
 }

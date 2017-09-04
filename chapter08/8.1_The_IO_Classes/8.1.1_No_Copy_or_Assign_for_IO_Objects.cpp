16adc0bcf81ca04c0cf601807488f54504317e4d
Bob <0haizhu0@gmail.com>
2017-09-04 16:07:42 +0800

    8.1.1 IO对象无拷贝或赋值

---
+++ chapter08/8.1_The_IO_Classes/8.1.1_No_Copy_or_Assign_for_IO_Objects.cpp
@@ -0,0 +1,16 @@
+/**
+ * 8.1.1 IO对象无拷贝或赋值
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/4
+ */
+
+//#include <fstream>
+
+int main(){
+    // ofstream out1, out2; // TODO 报错，应该引入哪个标准库？
+    // out1 = out2; // 错误：不能对流对象赋值
+    // ofstream print(ofstream); // 错误：不能初始化ofstream参数
+    // out2 = print(out2); // 错误：不能拷贝流对象
+    return 0;
+}

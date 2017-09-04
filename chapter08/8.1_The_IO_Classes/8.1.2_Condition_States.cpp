5398cc7e404a458e06f234826b4bcaaf74639514
Bob <0haizhu0@gmail.com>
2017-09-04 16:07:55 +0800

    8.1.2 条件状态

---
+++ chapter08/8.1_The_IO_Classes/8.1.2_Condition_States.cpp
@@ -0,0 +1,14 @@
+/**
+ * 8.1.2 条件状态
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/4
+ */
+int main(){
+    /**
+     * TODO
+     * 可以将流状态当做boolean表达式作为判断条件
+     * 也可以使用标准库方法获取流当前状态
+     */
+    return 0;
+}

ba480f38098c476914ef45f1cb2fc7ce594bd7f4
Bob <0haizhu0@gmail.com>
2017-09-04 16:07:29 +0800

    8.1 IO类

---
+++ chapter08/8.1_The_IO_Classes/8.1.0_introduction.cpp
@@ -0,0 +1,15 @@
+/**
+ * 8.1 IO类
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/4
+ */
+int main(){
+    /**
+     * 之前章节进行的IO操作都是操作char数据，其实，IO有如下三种数据类型的操作：
+     * iostream：用于读写流的基本类型
+     * fstream：用于读写命名文件的类型
+     * sstream：用于读写内存string对象的类型
+     */
+    return 0;
+}

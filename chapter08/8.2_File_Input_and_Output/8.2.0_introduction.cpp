43f3106bd080d152fd091ffb240af7ee9c1aced2
Bob <0haizhu0@gmail.com>
2017-09-06 22:44:41 +0800

    8.2 文件输入输出

---
+++ chapter08/8.2_File_Input_and_Output/8.2.0_introduction.cpp
@@ -0,0 +1,15 @@
+/**
+ * 8.2 文件输入输出
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/6
+ */
+int main(){
+  /**
+   * 头文件定义了三个类型来支持文件IO：
+   * ifstream    从一个给定文件读取数据
+   * ofstream    向一个给定文件写入数据
+   * fstream     可以读写给定文件
+   */
+  return 0;
+}

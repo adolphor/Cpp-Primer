4960be1736a6dd0a568b235ddb572fbe1deb4b9b
Bob <0haizhu0@gmail.com>
2017-09-06 22:50:49 +0800

    8.2 文件输入输出

---
+++ chapter08/8.2_File_Input_and_Output/8.2.1_Using_File_Stream_Objects.cpp
@@ -0,0 +1,10 @@
+/**
+ * 8.2.1 使用文件流对象
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/6
+ */
+int main(){
+  // TODO 需要的时候再看吧
+  return 0;
+}
---
+++ chapter08/8.2_File_Input_and_Output/8.2.2_File_Modes.cpp
@@ -0,0 +1,10 @@
+/**
+ * 8.2.2 文件模式
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/6
+ */
+int main(){
+  // TODO 需要的时候再看吧
+  return 0;
+}

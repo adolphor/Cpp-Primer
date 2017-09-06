0d66d41690673e40c732d55490803056bc95b95e
Bob <0haizhu0@gmail.com>
2017-09-06 22:56:27 +0800

    8.3 string 流

--- chapter08/8.3_string_Streams/8.3.0_introduction.cpp
+++ chapter08/8.3_string_Streams/8.3.0_introduction.cpp
@@ -5,5 +5,13 @@
  * @Date 2017/9/6
  */
 int main(){
+  /**
+   * sstream定义了三个类型来支持内存IO，可以从string读取数据，也可以向string写入数据，就像string是一个IO流一样。
+   *
+   * istringstream  从string读取数据
+   * ostringstream  向string写入数据
+   * stringstream   既可以读取又可以写入数据
+   *
+   */
   return 0;
 }

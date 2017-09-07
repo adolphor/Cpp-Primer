8f6a96c112b137840f88dc7be19720c153098c86
Bob <0haizhu0@gmail.com>
2017-09-07 23:00:38 +0800

    8.3.2 使用ostringstream

---
+++ chapter08/8.3_string_Streams/8.3.2_Using_ostringstreams.cpp
@@ -0,0 +1,13 @@
+/**
+ * 8.3.2 使用ostringstream
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/7
+ */
+int main(){
+  /**
+   * 当我们逐步构造输出，希望最后一起打印时，ostringstream是很有用的。
+   * TODO 需要的时候学习吧
+   */
+  return 0;
+}

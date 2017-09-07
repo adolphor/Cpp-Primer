94f764264dfb22a4ecc33ea91881f1bd5a28ef67
Bob <0haizhu0@gmail.com>
2017-09-07 22:56:05 +0800

    8.3.1 使用istringstream

---
+++ chapter08/8.3_string_Streams/8.3.1_Using_an_istringstream.cpp
@@ -0,0 +1,13 @@
+/**
+ * 8.3.1 使用istringstream
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/7
+ */
+int main(){
+  /**
+   * 当我们的工作是对整行文本进行处理，而其他一些工作是处理行内的单个单词时，通常可以使用istringstream。
+   * TODO 具体内容需要时学习
+   */
+  return 0;
+}

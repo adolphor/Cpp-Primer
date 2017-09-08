bf37f870175aa11d96b979f328e915ff8bc262da
Bob <0haizhu0@gmail.com>
2017-09-08 22:26:13 +0800

    9.1 顺序容器概述

---
+++ chapter09/9.1_Overview_of_the_Sequential_Containers/9.1.0_introduction.cpp
@@ -0,0 +1,23 @@
+/**
+ * 9.1 顺序容器概述
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/7
+ */
+int main(){
+  /**
+   * vector：可变大小数组
+   * deque：双端队列
+   * list：双向列表
+   * forward_list：单向列表
+   * array：固定大小数组
+   * string：与vector相似的容器，但专门用于保存字符
+   */
+
+  /**
+   * 容器的选择：
+   * 1. 除非你有很好的理由选择其他容器，否则应该使用vector
+   * 2. TODO 其他的原则以后再看吧
+   */
+  return 0;
+}

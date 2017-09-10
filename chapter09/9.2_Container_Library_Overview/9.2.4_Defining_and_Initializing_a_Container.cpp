d14f67f8e1ccbb77d83128646d8cc07ca77f99a1
Bob <0haizhu0@gmail.com>
2017-09-10 22:09:56 +0800

    9.2.4 容器定义和初始化

--- chapter09/9.2_Container_Library_Overview/9.2.4_Defining_and_Initializing_a_Container.cpp
+++ chapter09/9.2_Container_Library_Overview/9.2.4_Defining_and_Initializing_a_Container.cpp
@@ -10,14 +10,16 @@
 #include <list>
 #include <deque>
 #include <forward_list>
+#include <array>
 
 using std::string;
 using std::vector;
 using std::list;
 using std::deque;
 using std::forward_list;
+using std::array;
 
-int main(){
+int main() {
   /**
    * 每个容器类型都定义了一个默认构造函数，除array之外，其他容器的默认构造函数都会创建一个指定类型的空容器，、
    * 且都可以接受指定容器大小和元素初始值的参数。
@@ -30,7 +32,7 @@ int main(){
    */
   // 每个容器有三个元素，用给定的初始化器进行初始化
   list<string> authors = {"Milton", "Shakespeare", "Austen"};
-  vector<const char*> articles = {"a", "an", "the"};
+  vector<const char *> articles = {"a", "an", "the"};
   list<string> list2(authors); // ok: 类型匹配
   deque<string> authList(authors); // error: 容器类型不匹配
   vector<string> words(articles); // error: 元素类型必须匹配
@@ -42,7 +44,7 @@ int main(){
    */
   // 每个容器有三个元素，用给定的初始化器进行初始化
   list<string> authors = {"Milton", "Shakespeare", "Austen"};
-  vector<const char*> articles = {"a", "an", "the"};
+  vector<const char *> articles = {"a", "an", "the"};
 
   /**
    * 与顺序容器大小相关的构造函数
@@ -54,8 +56,21 @@ int main(){
 
   /**
    * 标准库array具有固定大小
+   * 与内置数组一样，标准库array的大小也是类型的一部分。当定义一个array时，除了指定元素类型，还要指定元素大小。
    */
+  array<int, 42>;                // 保存42个int的数组
+  array<string, 10>;             // 保存10个string的数组
 
+  // 为了使用array类型，我们必须同时指定元素类型和大小：
+  array<int, 10>::size_type i;   // 数组类型包括元素类型和大小
+
+  // 默认构造函数
+  array<int, 10> ia2 = {0,1,2,3,4,5,6,7,8,9}; // 列表初始化
+  array<int, 10> ia3 = {42};    // ia3[0]为42，其余为0
+
+  // array支持拷贝
+  array<int, 10> digits = {0,1,2,3,4,5,6,7,8,9};
+  array<int, 10> copy = digits;
 
   return 0;
 }

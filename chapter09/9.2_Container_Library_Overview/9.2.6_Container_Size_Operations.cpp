c6da07787da43e6aa028646cb54f83dc96837db9
Bob <0haizhu0@gmail.com>
2017-09-15 23:36:26 +0800

    9.2.6 容器大小操作

---
+++ chapter09/9.2_Container_Library_Overview/9.2.6_Container_Size_Operations.cpp
@@ -0,0 +1,17 @@
+/**
+ * 9.2.6 容器大小操作
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/15
+ */
+int main(){
+  /**
+   * 除了一个例外，每个容器类型都有三个与大小相关的操作。
+   * 1. 成员函数size返回容器中元素的数目
+   * 2. empty当size为0的时候为true，否则返回false
+   * 3. max_size返回一个等于或大于该类型容器所能容纳的最大元素数的值
+   *
+   * forward_list支持max_size和empty，但不支持size。下节讲述原因。
+   */
+  return 0;
+}

fcca7b2b54e89b834d42fb4243080749c78d6000
Bob <0haizhu0@gmail.com>
2017-09-18 23:51:07 +0800

    9.3.1 向顺序容器添加元素

---
+++ chapter09/9.3_Sequential_Container_Operations/9.3.1_Adding_Elements_to_a_Sequential_Container.cpp
@@ -0,0 +1,16 @@
+/**
+ * 9.3.1 向顺序容器添加元素
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/18
+ */
+int main(){
+  /**
+   * 使用push_back：除了array和forward_list每个顺序容器都支持push_back，作用是将一个元素放入顺序容器尾部。
+   * 当我们用一个对象来初始化容器，或将一个对象插入到容器中时，实际上放入到容器中的是对象值的拷贝，而不是对象本身。
+   *
+   */
+
+
+  return 0;
+}

ba76edcbd1fb0b2600522a7f8a3d240891f9c8d9
Bob <0haizhu0@gmail.com>
2017-09-20 14:19:23 +0800

    9.5 额外的string操作

---
+++ chapter09/9.5_Additional_string_Operations/9.5.0_introduction.cpp
@@ -0,0 +1,14 @@
+/**
+ * 9.5 额外的string操作
+ * @Author Bob
+ * @Eamil 0haizhu0@gmail.com
+ * @Date 2017/9/20
+ */
+int main(){
+ /**
+  * 除了顺序容器共同的操作之外，string类型还提供了一些额外的操作。
+  * 1. 提供string类和C风格字符数组之间的相互转换
+  * 2. 增加了允许我们用下标代替迭代器的版本
+  */
+  return 0;
+}

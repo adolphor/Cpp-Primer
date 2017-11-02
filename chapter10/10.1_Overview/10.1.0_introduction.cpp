52acd37e87366cd0a7ef0c1c882b4a040fd33068
Bob <0haizhu0@gmail.com>
2017-11-02 22:46:58 +0800

    10.1 概述

--- chapter10/10.1_Overview/10.1.0_introduction.cpp
+++ chapter10/10.1_Overview/10.1.0_introduction.cpp
@@ -4,7 +4,31 @@
  * @Eamil 0haizhu0@gmail.com
  * @Date 2017/9/20
  */
-int main(){
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  /**
+   * 大多数算法并不直接操作容器，而是遍历由两个迭代器指定的一个元素范围来进行操作。
+   * 迭代器令算法不依赖于容器，但算法依赖于元素类型的操作。
+   * 算法永远不会执行容器的操作：可以改变或者移动元素，但永远不会改变容器的大小。
+   */
+  int val = 42;// 要查找的元素,类型要与vector<>类型一致
+
+  vector<int> vec;
+  for (int i = 0; i < 10; i++) {
+    vec.push_back(i * i);
+  }
+
+  auto iter = std::find(vec.begin(), vec.end(), val);// 返回的是一个迭代器指针，类型是 vector<int>::iterator
+
+  if (iter == vec.end())
+    cout << "ERROR!" << endl;
+  else               // 注意迭代器指针输出元素的方式和distance用法
+    cout << "the index of value " << (*iter) << " is " << std::distance(vec.begin(), iter) << std::endl;
   return 0;
+
 }
